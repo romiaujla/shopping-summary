480c7688f9669c4a305fc7c2fb01ecf3db4729d4
Raman Aujla <ramanpreet.aujla@gmail.com>
2020-02-02 13:20:37 -0500

    added style to app outer layer

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import { Container } from 'react-bootstrap';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <h1>App Component</h1>
+    <div className='Container'>
+      <Container className='purchase-card'>
+        <h1>App</h1>
+      </Container>
     </div>
   );
 }
