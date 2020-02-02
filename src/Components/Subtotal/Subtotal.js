ea757cbbd122323a16fe25a99854e342b531080e
Raman Aujla <ramanpreet.aujla@gmail.com>
2020-02-02 13:28:34 -0500

    initialized component

---
+++ src/Components/Subtotal/Subtotal.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class Subtotal extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {  }
+    }
+    render() { 
+        return (
+            <div className='Subtotal'>
+                Subtotal
+            </div>
+        );
+    }
+}
+ 
+export default Subtotal;
